2db0cb78a660751d16f592107eda6beadbd74a5b
alinsimy <alinsimy@gmail.com>
2017-06-18 16:20:30 +0300

    Circles collision detect util function added

--- src/js/utils.js
+++ src/js/utils.js
@@ -3,3 +3,14 @@ function getRandomInt(min, max) {
 	max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min)) + min;
 }
+
+function detectCirclesCollision(obiect1, object2) {
+    var dx = obiect1.x - obiect2.x;
+    var dy = obiect1.y - obiect2.y;
+    var distance = Math.sqrt(dx * dx + dy * dy);
+
+    if (distance < obiect1.radius + obiect2.radius) {
+        return true;
+    }
+    return false;
+}
